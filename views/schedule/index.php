d11d9b4b1d5452c0c97b491e4ce3d2b0e4d88740
Evgeny Belkin <evbelkin@localhost.localdomain>
2018-09-03 20:48:28 +0300

    добавлены актуальные сборки приложения расписание

--- views/schedule/index.php
+++ views/schedule/index.php
@@ -3,12 +3,14 @@ $this->title = 'Система учета :: ' . Yii::t('app', 'Schedule');
 $this->params['breadcrumbs'][] = Yii::t('app','Schedule');
 ?>
 
-<div id="schedule-root" class="row">
+<div id="app" class="row">
   <c-sidebar :user="user"></c-sidebar>
   <c-content></c-content>
 </div>
 
 <?php
-    $this->registerJsFile('/js/schedule/bundle.js',
-    ['position' => yii\web\View::POS_END]);
+    $this->registerCssFile('/css/chunk-vendors.f39c34b7.css');
+    $this->registerCssFile('/css/app.1cf9e0a6.css');
+    $this->registerJsFile('/js/chunk-vendors.5aa52ffe.js');
+    $this->registerJsFile('/js/app.c76b92a9.js');
 ?>
