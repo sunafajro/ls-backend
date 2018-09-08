150152623481759aceb3e62000dfe863fab447f6
Evgeny Belkin <sunafajro@gmail.com>
2018-09-08 21:41:45 +0300

    добавлены новые сборки приложения расписание, изменены имена файлов на универсальные

--- views/schedule/index.php
+++ views/schedule/index.php
@@ -9,8 +9,8 @@ $this->params['breadcrumbs'][] = Yii::t('app','Schedule');
 </div>
 
 <?php
-    $this->registerCssFile('/css/chunk-vendors.f39c34b7.css');
-    $this->registerCssFile('/css/app.1cf9e0a6.css');
-    $this->registerJsFile('/js/chunk-vendors.5aa52ffe.js');
-    $this->registerJsFile('/js/app.c76b92a9.js');
+    $this->registerCssFile('/css/schedule/vendors.css');
+    $this->registerCssFile('/css/schedule/app.css');
+    $this->registerJsFile('/js/schedule/vendors.js');
+    $this->registerJsFile('/js/schedule/app.js');
 ?>
