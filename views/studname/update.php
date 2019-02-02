828c9f9042487db478a5f6d50893d3be9a83071f
Evgeny Belkin <sunafajro@gmail.com>
2019-02-02 21:58:53 +0300

    форматирование кода

--- views/studname/update.php
+++ views/studname/update.php
@@ -2,14 +2,12 @@
 
 use yii\helpers\Html;
 
-/* @var $this yii\web\View */
-/* @var $model app\models\CalcStudname */
-
 $this->title = Yii::t('app', 'Update client') . ': ' . ' ' . $model->name;
 $this->params['breadcrumbs'][] = ['label' => Yii::t('app', 'Clients'), 'url' => ['index']];
 $this->params['breadcrumbs'][] = ['label' => $model->name, 'url' => ['view', 'id' => $model->id]];
 $this->params['breadcrumbs'][] = Yii::t('app','Update');
 ?>
+
 <div class="row row-offcanvas row-offcanvas-left student-update">
     <div id="sidebar" class="col-xs-6 col-sm-2 sidebar-offcanvas">
 		<?= $userInfoBlock ?>
@@ -22,10 +20,10 @@ $this->params['breadcrumbs'][] = Yii::t('app','Update');
 		<p class="pull-left visible-xs">
 			<button type="button" class="btn btn-primary btn-xs" data-toggle="offcanvas">Toggle nav</button>
 		</p>
-    <?= $this->render('_form', [
-        'model' => $model,
-        'sex' => $sex,
-        'way' => $way,
-    ]) ?>
+		<?= $this->render('_form', [
+			'model' => $model,
+			'sex' => $sex,
+			'way' => $way,
+		]) ?>
     </div>
 </div>
