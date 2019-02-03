935881ddf2af297d7e5b1f02d6398ad3af55c81e
Evgeny Belkin <sunafajro@gmail.com>
2019-02-03 17:03:10 +0300

    добавляем глобальную переменную для отображения нужного режима приложения

--- assets/AppAsset.php
+++ assets/AppAsset.php
@@ -1,37 +1,54 @@
 <?php
-/**
- * @link http://www.yiiframework.com/
- * @copyright Copyright (c) 2008 Yii Software LLC
- * @license http://www.yiiframework.com/license/
- */
+    namespace app\assets;
+    use Yii;
+    use yii\web\AssetBundle;
 
-namespace app\assets;
-
-use yii\web\AssetBundle;
-
-/**
- * @author Qiang Xue <qiang.xue@gmail.com>
- * @since 2.0
- */
-class AppAsset extends AssetBundle
-{
-    public $basePath = '@webroot';
-    public $baseUrl = '@web';
-    public $css = [
-        'css/offcanvas.css',
-        'fa/css/font-awesome.min.css',
-        'https://cdnjs.cloudflare.com/ajax/libs/noty/3.1.4/noty.min.css',
-        'https://cdn.jsdelivr.net/npm/suggestions-jquery@17.10.1/dist/css/suggestions.min.css',
-        'css/site.css',
-    ];
-    public $js = [
-        'js/offcanvas.js',
-        'https://cdnjs.cloudflare.com/ajax/libs/jquery-ajaxtransport-xdomainrequest/1.0.1/jquery.xdomainrequest.min.js',
-        'https://cdn.jsdelivr.net/npm/suggestions-jquery@17.10.1/dist/js/jquery.suggestions.min.js'
-    ];
-    public $depends = [
-        'yii\web\YiiAsset',
-        'yii\bootstrap\BootstrapAsset',
-        'yii\bootstrap\BootstrapPluginAsset',
-    ];
-}
+    if (Yii::$app->params['appMode'] === 'bitrix') {
+        class AppAsset extends AssetBundle
+        {
+            public $basePath = '@webroot';
+            public $baseUrl = '@web';
+            public $css = [
+                'css/offcanvas.css',
+                'fa/css/font-awesome.min.css',
+                'https://cdnjs.cloudflare.com/ajax/libs/noty/3.1.4/noty.min.css',
+                'https://cdn.jsdelivr.net/npm/suggestions-jquery@17.10.1/dist/css/suggestions.min.css',
+                'css/site.css',
+            ];
+            public $js = [
+                'js/offcanvas.js',
+                'https://cdnjs.cloudflare.com/ajax/libs/jquery-ajaxtransport-xdomainrequest/1.0.1/jquery.xdomainrequest.min.js',
+                'https://cdn.jsdelivr.net/npm/suggestions-jquery@17.10.1/dist/js/jquery.suggestions.min.js',
+                '/js/menu/vendors.js',
+                '/js/menu/bundle.js'
+            ];
+            public $depends = [
+                'yii\web\YiiAsset',
+                'yii\bootstrap\BootstrapAsset',
+                'yii\bootstrap\BootstrapPluginAsset',
+            ];
+        }
+    } else {
+        class AppAsset extends AssetBundle
+        {
+            public $basePath = '@webroot';
+            public $baseUrl = '@web';
+            public $css = [
+                'css/offcanvas.css',
+                'fa/css/font-awesome.min.css',
+                'https://cdnjs.cloudflare.com/ajax/libs/noty/3.1.4/noty.min.css',
+                'https://cdn.jsdelivr.net/npm/suggestions-jquery@17.10.1/dist/css/suggestions.min.css',
+                'css/site.css',
+            ];
+            public $js = [
+                'js/offcanvas.js',
+                'https://cdnjs.cloudflare.com/ajax/libs/jquery-ajaxtransport-xdomainrequest/1.0.1/jquery.xdomainrequest.min.js',
+                'https://cdn.jsdelivr.net/npm/suggestions-jquery@17.10.1/dist/js/jquery.suggestions.min.js'
+            ];
+            public $depends = [
+                'yii\web\YiiAsset',
+                'yii\bootstrap\BootstrapAsset',
+                'yii\bootstrap\BootstrapPluginAsset',
+            ];
+        }
+    }
