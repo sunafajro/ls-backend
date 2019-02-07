ecf0f789c284fda7a20ed0fecc777e211a6a74f5
Evgeny Belkin <sunafajro@gmail.com>
2019-02-08 00:01:54 +0300

    актуальное приложение для отчета по зарплатам

--- views/report/salaries.php
+++ views/report/salaries.php
@@ -1,7 +1,5 @@
 <?php
   $this->title = 'Система учета :: '.Yii::t('app','Reports');
-  $this->params['breadcrumbs'][] = ['label' => Yii::t('app','Reports'), 'url' => '/report/index'];
-  $this->params['breadcrumbs'][] = Yii::t('app','Salaries');
 ?>
 
 <div id="app" data-nullyear="2011"></div>
