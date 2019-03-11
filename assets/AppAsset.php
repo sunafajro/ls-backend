b4506b13fb1eb1e6578992aa2f0740b3a9c65e54
Evgeny Belkin <sunafajro@gmail.com>
2019-03-11 22:34:22 +0300

    переписан класс более компактно

--- assets/AppAsset.php
+++ assets/AppAsset.php
@@ -1,54 +1,45 @@
 <?php
-    namespace app\assets;
-    use Yii;
-    use yii\web\AssetBundle;
+namespace app\assets;
 
-    if (Yii::$app->params['appMode'] === 'bitrix') {
-        class AppAsset extends AssetBundle
-        {
-            public $basePath = '@webroot';
-            public $baseUrl = '@web';
-            public $css = [
-                'css/offcanvas_bitrix.css',
-                'fa/css/font-awesome.min.css',
-                'https://cdnjs.cloudflare.com/ajax/libs/noty/3.1.4/noty.min.css',
-                'https://cdn.jsdelivr.net/npm/suggestions-jquery@17.10.1/dist/css/suggestions.min.css',
-                'css/site_bitrix.css',
-            ];
-            public $js = [
-                'js/offcanvas.js',
-                'https://cdnjs.cloudflare.com/ajax/libs/jquery-ajaxtransport-xdomainrequest/1.0.1/jquery.xdomainrequest.min.js',
-                'https://cdn.jsdelivr.net/npm/suggestions-jquery@17.10.1/dist/js/jquery.suggestions.min.js',
-                '/js/menu/vendors.js',
-                '/js/menu/bundle.js'
-            ];
-            public $depends = [
-                'yii\web\YiiAsset',
-                'yii\bootstrap\BootstrapAsset',
-                'yii\bootstrap\BootstrapPluginAsset',
-            ];
-        }
-    } else {
-        class AppAsset extends AssetBundle
-        {
-            public $basePath = '@webroot';
-            public $baseUrl = '@web';
-            public $css = [
-                'css/offcanvas_standalone.css',
-                'fa/css/font-awesome.min.css',
-                'https://cdnjs.cloudflare.com/ajax/libs/noty/3.1.4/noty.min.css',
-                'https://cdn.jsdelivr.net/npm/suggestions-jquery@17.10.1/dist/css/suggestions.min.css',
-                'css/site_standalone.css',
-            ];
-            public $js = [
-                'js/offcanvas.js',
-                'https://cdnjs.cloudflare.com/ajax/libs/jquery-ajaxtransport-xdomainrequest/1.0.1/jquery.xdomainrequest.min.js',
-                'https://cdn.jsdelivr.net/npm/suggestions-jquery@17.10.1/dist/js/jquery.suggestions.min.js'
-            ];
-            public $depends = [
-                'yii\web\YiiAsset',
-                'yii\bootstrap\BootstrapAsset',
-                'yii\bootstrap\BootstrapPluginAsset',
-            ];
-        }
+use Yii;
+use yii\web\AssetBundle;
+
+class AppAsset extends AssetBundle
+{
+    public $basePath = '@webroot';
+    public $baseUrl = '@web';
+    public $css = [];
+    public $js = [];
+    public $depends = [
+        'yii\web\YiiAsset',
+        'yii\bootstrap\BootstrapAsset',
+        'yii\bootstrap\BootstrapPluginAsset',
+    ];
+    public function __construct()
+    {
+        $this->css = Yii::$app->params['appMode'] === 'bitrix' ? [
+            'css/offcanvas_bitrix.css',
+            'fa/css/font-awesome.min.css',
+            'https://cdnjs.cloudflare.com/ajax/libs/noty/3.1.4/noty.min.css',
+            'https://cdn.jsdelivr.net/npm/suggestions-jquery@17.10.1/dist/css/suggestions.min.css',
+            'css/site_bitrix.css',
+        ] : [
+            'css/offcanvas_standalone.css',
+            'fa/css/font-awesome.min.css',
+            'https://cdnjs.cloudflare.com/ajax/libs/noty/3.1.4/noty.min.css',
+            'https://cdn.jsdelivr.net/npm/suggestions-jquery@17.10.1/dist/css/suggestions.min.css',
+            'css/site_standalone.css',
+        ];
+        $this->js = Yii::$app->params['appMode'] === 'bitrix' ? [
+            'js/offcanvas.js',
+            'https://cdnjs.cloudflare.com/ajax/libs/jquery-ajaxtransport-xdomainrequest/1.0.1/jquery.xdomainrequest.min.js',
+            'https://cdn.jsdelivr.net/npm/suggestions-jquery@17.10.1/dist/js/jquery.suggestions.min.js',
+            '/js/menu/vendors.js',
+            '/js/menu/bundle.js',
+        ] : [
+            'js/offcanvas.js',
+            'https://cdnjs.cloudflare.com/ajax/libs/jquery-ajaxtransport-xdomainrequest/1.0.1/jquery.xdomainrequest.min.js',
+            'https://cdn.jsdelivr.net/npm/suggestions-jquery@17.10.1/dist/js/jquery.suggestions.min.js',
+        ];
     }
+}
