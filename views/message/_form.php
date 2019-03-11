d3536a48d3fd4694b586b43a4ea137120de107ed
Evgeny Belkin <sunafajro@gmail.com>
2019-03-11 21:41:43 +0300

    форматирование кода

--- views/message/_form.php
+++ views/message/_form.php
@@ -1,50 +1,50 @@
 <?php
-  use yii\helpers\Html;
-  use yii\widgets\ActiveForm;
-  use moonland\tinymce\TinyMCE;
+use moonland\tinymce\TinyMCE;
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
 ?>
 
 <div class="calc-message-form">
-    <?php $form = ActiveForm::begin(); ?>
-    <?= $form->field($model, 'name')->textInput() ?>
-	<?= $form->field($model, 'description')->widget(TinyMCE::className(), [
-        'toggle' => [
-            'active' => false,
-        ],
-        'statusbar' => false,
-        'menubar' => false,
-        'paste_as_text' => true,
-        'toolbar' => [
-            'fontsizes',
-            'bold',
-            'italic',
-            'underline',
-            'alignleft',
-            'aligncenter',
-            'alignright',
-            'alignjustify',
-            'textcolor',
-            'backgroundcolor',
-            'bullist',
-            'numlist',
-            'link',
-            'unlink',
-            'image',
-            'removeformat'
-        ],
-    ]);
-	?>
-    <?= $form->field($model, 'calc_messwhomtype')->dropDownList($item=$types, ['prompt' => Yii::t('app', '-select-'), 'disabled' => $model->isNewRecord ? false : true]) ?>
-    <?= $form->field($model, 'refinement')->hiddenInput(['value'=>0])->label(false) ?>
-    <?= $form->field($model, 'refinement_id')->hiddenInput(['value'=>0])->label(false) ?>
+    <?php $form = ActiveForm::begin();?>
+    <?=$form->field($model, 'name')->textInput()?>
+	<?=$form->field($model, 'description')->widget(TinyMCE::className(), [
+    'toggle' => [
+        'active' => false,
+    ],
+    'statusbar' => false,
+    'menubar' => false,
+    'paste_as_text' => true,
+    'toolbar' => [
+        'fontsizes',
+        'bold',
+        'italic',
+        'underline',
+        'alignleft',
+        'aligncenter',
+        'alignright',
+        'alignjustify',
+        'textcolor',
+        'backgroundcolor',
+        'bullist',
+        'numlist',
+        'link',
+        'unlink',
+        'image',
+        'removeformat',
+    ],
+]);
+?>
+    <?=$form->field($model, 'calc_messwhomtype')->dropDownList($item = $types, ['prompt' => Yii::t('app', '-select-'), 'disabled' => $model->isNewRecord ? false : true])?>
+    <?=$form->field($model, 'refinement')->hiddenInput(['value' => 0])->label(false)?>
+    <?=$form->field($model, 'refinement_id')->hiddenInput(['value' => 0])->label(false)?>
     <div class="form-group">
-        <?= Html::submitButton($model->isNewRecord ? Yii::t('app','Add') : Yii::t('app','Update'), ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
+        <?=Html::submitButton($model->isNewRecord ? Yii::t('app', 'Add') : Yii::t('app', 'Update'), ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary'])?>
     </div>
-    <?php ActiveForm::end(); ?>
+    <?php ActiveForm::end();?>
 
 </div>
 
-<?php 
+<?php
 $this->registerJs('$(document).ready(
 function(){
     $("#message-calc_messwhomtype").change(
