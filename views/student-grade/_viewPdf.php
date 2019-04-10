a44b0a03e33997d89563c68b2d2cec0371a35429
Evgeny Belkin <sunafajro@gmail.com>
2019-04-11 01:07:51 +0300

    Улучшено оформление аттестата

--- views/student-grade/_viewPdf.php
+++ views/student-grade/_viewPdf.php
@@ -7,15 +7,37 @@
 
 use Yii;
 ?>
-<p>
-    <b><?= Yii::t('app', 'Date') ?>:</b> <?= date('d.m.Y', strtotime($attestation['date'])) ?>
-</p>
-<p>
-    <b><?= Yii::t('app', 'Description') ?>:</b> <?= $attestation['description'] ?>
-</p>
-<p>
-    <b><?= Yii::t('app', 'Score') ?>:</b> <?= $attestation['score'] ?>
-</p>
-<p>
-    <b><?= Yii::t('app', 'Student') ?>:</b> <?= $attestation['studentName'] ?>
-</p>
+<div style="padding: 2rem 5rem">
+    <div style="text-align: center; font-size: 18px; margin-bottom: 2rem">
+        Негосударственное образовательное учреждение дополнительного и дополнительного профессионального образования
+    </div>
+    <div style="text-align: center; font-size: 18px; margin-bottom: 3rem">
+        Школа иностранных языков "Язык для успеха"
+    </div>
+    <div style="margin-bottom: 3rem; font-size: 18px;">
+        Дата: <?= date('d.m.Y', strtotime($attestation['date'])) ?><br />
+        Регистрационный номер: <?= date('ymd', strtotime($attestation['date'])) . '-' . $attestation['id'] ?><br />
+        г. Чебоксары<br />
+    </div>
+    <div style="margin-left: 2rem; margin-right: 2rem; margin-bottom: 1rem; font-size: 18px;">
+        Настоящим удостоверяется, что
+    </div>
+    <div style="margin-left: 2rem; margin-right: 2rem; text-align: center; margin-bottom: 1rem; font-size: 24px; font-style: italic; border-bottom: 1px solid #999999">
+        <?= $attestation['studentName'] ?>
+    </div>
+    <div style="margin-left: 2rem; margin-right: 2rem; margin-bottom: 1rem; font-size: 18px;">
+        сдал
+    </div>
+    <div style="margin-left: 2rem; margin-right: 2rem; text-align: center; margin-bottom: 1rem; font-size: 24px; font-style: italic; border-bottom: 1px solid #999999">
+        <?= $attestation['description'] ?>
+    </div>
+    <div style="margin-left: 2rem; margin-right: 2rem; margin-bottom: 1rem; font-size: 18px;">
+        и получил
+    </div>
+    <div style="margin-left: 2rem; margin-right: 2rem; text-align: center; font-size: 24px; font-style: italic; border-bottom: 1px solid #999999">
+        <?= $attestation['score'] ?>
+    </div>
+    <div style="position: fixed; bottom: 2rem">
+        Лицензия Министерства образования ЧР № 799 от 27.02.2014 г. серия 21ЛО1 № 0000152
+    </div>
+</div>
