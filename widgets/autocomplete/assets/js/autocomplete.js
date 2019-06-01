88d82ef825257ecd71a3a41e4533aca74198a81a
Evgeny Belkin <sunafajro@gmail.com>
2019-06-02 01:27:13 +0300

    #57 Строка поиска отсылается в POST, нет ограничения на мин колич символов

--- widgets/autocomplete/assets/js/autocomplete.js
+++ widgets/autocomplete/assets/js/autocomplete.js
@@ -7,35 +7,36 @@ $(document).ready(function() {
     .on("input", function() {
       var $this = $(this);
       var str = $this.val();
-      if (str.length > 2) {
-        if (!loading) {
-          loading = true;
-          $.ajax({
-            method: "POST",
-            url: $this.data("url") + "?term=" + str
-          }).done(function(result) {
-            $studentListBlock.html("");
-            if (Array.isArray(result) && result.length) {
-              foundStudents = result;
-              foundStudents.forEach(function(student) {
-                $studentListBlock.append(
-                  '<li data-id="' +
-                    student["value"].trim() +
-                    '">' +
-                    student["label"].trim() +
-                    "</li>"
-                );
-              });
-              $studentListBlock.show();
-              $("#js--autocomplete-list li").on("click", function() {
-                var $this = $(this);
-                $studentField.val($this.data("id"));
-                $("#js--autocomplete").val($this.text());
-              });
-            }
-            loading = false;
-          });
-        }
+      if (!loading) {
+        loading = true;
+        $.ajax({
+          method: "POST",
+          url: $this.data("url"),
+          data: { term: str }
+        }).done(function(result) {
+          $studentListBlock.html("");
+          if (Array.isArray(result) && result.length) {
+            foundStudents = result;
+            foundStudents.forEach(function(student) {
+              $studentListBlock.append(
+                '<li data-id="' +
+                  student["value"].trim() +
+                  '">' +
+                  student["label"].trim() +
+                  "</li>"
+              );
+            });
+            $studentListBlock.show();
+            $("#js--autocomplete-list li").on("click", function() {
+              var $this = $(this);
+              $studentField.val($this.data("id"));
+              $("#js--autocomplete").val($this.text());
+            });
+          } else {
+            $studentListBlock.hide();
+          }
+          loading = false;
+        });
       }
     })
     .on("focusout", function() {
