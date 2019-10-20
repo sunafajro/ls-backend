241b10c94633f8d3f7b76c2146cfd024e376e402
Evgeny Belkin <sunafajro@gmail.com>
2019-10-20 16:17:36 +0300

    #106 Механизм назначения основного преподавателя группы.

--- views/groupteacher/addteacher.php
+++ views/groupteacher/addteacher.php
@@ -1,24 +1,35 @@
 <?php
 
+use app\models\Groupteacher;
+use app\widgets\Alert;
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
+use yii\web\View;
 
-/* @var $this yii\web\View */
-/* @var $model app\models\CalcTeachergroup */
-/* @var $form yii\widgets\ActiveForm */
+/**
+ * @var View         $this
+ * @var ActiveForm   $form
+ * @var Groupteacher $group
+ * @var array        $teachers
+ * @var string       $userInfoBlock
+ */
 
-$this->title = 'Система учета :: ' . Yii::t('app', 'List of teachers');
+$this->title = Yii::$app->params['appTitle'] . Yii::t('app', 'List of teachers');
 $this->params['breadcrumbs'][] = ['label' => Yii::t('app', 'Group').' №' . $params['gid'], 'url' => ['groupteacher/view', 'id' => $params['gid']]];
 $this->params['breadcrumbs'][] = Yii::t('app', 'List of teachers');
-
+$roleId = Yii::$app->session->get('user.ustatus');
 ?>
 
 <div class="row row-offcanvas row-offcanvas-left group-add-teacher">
     <div id="sidebar" class="col-xs-6 col-sm-2 sidebar-offcanvas">
         <?= $userInfoBlock ?>
         <?php if($params['active'] == 1): ?>
-            <?php if(Yii::$app->session->get('user.ustatus')==3 || Yii::$app->session->get('user.ustatus')==4 || array_key_exists(Yii::$app->session->get('user.uteacher'), $check_teachers)): ?>
-                <?= Html::a('<span class="fa fa-plus" aria-hidden="true"></span> '.Yii::t('app','Add lesson'), ['journalgroup/create','gid' => $params['gid']], ['class' => 'btn btn-default btn-block']) ?>
+            <?php if(in_array($roleId, [3, 4]) || array_key_exists(Yii::$app->session->get('user.uteacher'), $check_teachers)): ?>
+                <?= Html::a(
+                        Html::tag('span', '', ['class' => 'fa fa-plus', 'aria-hidden' => 'true']) . Yii::t('app','Add lesson'),
+                        ['journalgroup/create','gid' => $params['gid']],
+                        ['class' => 'btn btn-default btn-block']
+                    ) ?>
             <?php endif; ?>        
             <?php foreach($items as $item): ?>
                 <?= Html::a($item['title'], $item['url'], $item['options']) ?>
@@ -39,31 +50,22 @@ $this->params['breadcrumbs'][] = Yii::t('app', 'List of teachers');
 	<div class="col-sm-10">
 		<p class="pull-left visible-xs">
 			<button type="button" class="btn btn-primary btn-xs" data-toggle="offcanvas">Toggle nav</button>
-		</p>
-		<?php if(Yii::$app->session->hasFlash('error')): ?>
-			<div class="alert alert-danger" role="alert">
-				<?= Yii::$app->session->getFlash('error') ?>
-			</div>
-		<?php endif; ?>
+        </p>
+        
+        <?= Alert::widget() ?>     
 
-		<?php if(Yii::$app->session->hasFlash('success')): ?>
-			<div class="alert alert-success" role="alert">
-				<?= Yii::$app->session->getFlash('success') ?>
-			</div>
-		<?php endif; ?>        
+        <?php if (in_array($roleId, [3, 4])) { ?>
+            <h4><?= Yii::t('app', 'Add teacher to group') . ' #' . $params['gid'] ?></h4>
+            <?php $form = ActiveForm::begin(); ?>
 
-        <?php if(Yii::$app->session->get('user.ustatus')==3||Yii::$app->session->get('user.ustatus')==4): ?>
-        <h4><?= Yii::t('app', 'Add teacher to group') . ' #' . $params['gid'] ?></h4>
-        <?php $form = ActiveForm::begin(); ?>
+            <?= $form->field($model, 'calc_teacher')->dropDownList($items=$teachers, ['prompt'=>Yii::t('app', '-select-')]) ?>
 
-        <?= $form->field($model, 'calc_teacher')->dropDownList($items=$teachers, ['prompt'=>Yii::t('app', '-select-')]) ?>
+            <div class="form-group">
+                <?= Html::submitButton(Yii::t('app', 'Add'), ['class' => 'btn btn-success']) ?>
+            </div>
 
-        <div class="form-group">
-            <?= Html::submitButton(Yii::t('app', 'Add'), ['class' => 'btn btn-success']) ?>
-        </div>
-
-        <?php ActiveForm::end(); ?>
-        <?php endif; ?>     
+            <?php ActiveForm::end(); ?>
+        <?php } ?>     
 	
         <h4><?php echo Yii::t('app', 'Current teachers of the group'); ?></h4>
         <table class="table">
@@ -75,33 +77,62 @@ $this->params['breadcrumbs'][] = Yii::t('app', 'List of teachers');
                     <th><?php echo Yii::t('app', 'Actions'); ?></th>
                 </tr>
             <tbody>
-            <?php foreach($curteachers as $curteacher): ?>
-                <?php switch($curteacher['visible']){
+            <?php foreach ($curteachers as $curteacher) { ?>
+                <?php switch ($curteacher['visible']) {
                     case 0: echo "<tr class='danger'>"; break;
                     case 1: echo "<tr class='success'>"; break;
                     default: echo "<tr>"; break;
                 } ?>
                 <td>
-                <?php if(Yii::$app->session->get('user.ustatus')==3||Yii::$app->session->get('user.ustatus')==4): ?>
-                    <?= Html::a($curteacher['teacher'],['teacher/view','id'=>$curteacher['id']]) ?>
-                <?php elseif(Yii::$app->session->get('user.ustatus')==5&&Yii::$app->session->get('user.uteacher')==$curteacher['id']):  ?>
-                    <?= Html::a($curteacher['teacher'],['teacher/view','id'=>$curteacher['id']]) ?>
-                <?php else:  ?>
+                <?php if (in_array($roleId, [3, 4])) { ?>
+                    <?= Html::a($curteacher['teacher'], ['teacher/view','id' => $curteacher['id']]) ?>
+                <?php } else {  ?>
                     <?= $curteacher['teacher'] ?>
-                <?php endif; ?>
+                <?php } ?>
+                    <?= (int)$group->calc_teacher === (int)$curteacher['id']
+                            ? Html::tag('span', '', ['class' => 'fa fa-star', 'aria-hidden' => 'true', 'title' => 'Основной преподаватель'])
+                            : '' ?>
                 </td>
                 <td><?= $curteacher['date'] ?></td>
                 <td><?= $curteacher['user'] ?></td>
                 <td>
-                <?php if(Yii::$app->session->get('user.ustatus')==3 || Yii::$app->session->get('user.ustatus')==4): ?>
-                    <?php switch($curteacher['visible']){
-                        case 0: echo Html::a('', ['groupteacher/restoreteacher','gid' => $params['gid'],'tid'=>$curteacher['id']],['class'=>'glyphicon glyphicon-ok']);break;
-                        case 1: echo Html::a('', ['groupteacher/delteacher','gid' => $params['gid'],'tid'=>$curteacher['id']],['class'=>'glyphicon glyphicon-remove']);break;
-                        default: echo "";break;
+                <?php if (in_array($roleId, [3, 4])) { ?>
+                    <?= (int)$group->calc_teacher !== (int)$curteacher['id']
+                            ? Html::a(
+                                '',
+                                ['groupteacher/set-primary-teacher', 'id' => $params['gid'], 'tid' => $curteacher['id']],
+                                [
+                                    'class' => 'fa fa-star-o',
+                                    'title' => 'Назначить основным',
+                                ]
+                            ) : '' ?>
+                    <?php switch ($curteacher['visible']) {
+                        case 0:
+                            echo Html::a(
+                                    '',
+                                    ['groupteacher/restoreteacher', 'gid' => $params['gid'], 'tid' => $curteacher['id']],
+                                    [
+                                        'class' => 'fa fa-check',
+                                        'title' => 'Восстановить преподавателя',
+                                    ]
+                                );
+                            break;
+                        case 1:
+                            echo Html::a(
+                                    '',
+                                    ['groupteacher/delteacher', 'gid' => $params['gid'], 'tid' => $curteacher['id']],
+                                    [
+                                        'class' => 'fa fa-times',
+                                        'title' => 'Удалить преподавателя',
+                                    ]
+                                );
+                            break;
+                        default:
+                            echo '';
                     } ?>
-                <?php endif; ?>
+                <?php } ?>
                 </td>
-            <?php endforeach; ?>
+                <?php } ?>
             </tbody>
         </table>
     </div>
