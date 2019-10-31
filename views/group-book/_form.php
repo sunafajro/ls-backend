5a89b9b683ef0e27bfd2dccf46015173557e9952
Evgeny Belkin <sunafajro@gmail.com>
2019-10-31 23:30:01 +0300

    #86 Добавление/удаление учебника из группы

--- views/group-book/_form.php
+++ views/group-book/_form.php
@@ -1,47 +1,23 @@
 <?php
 
+use app\models\GroupBook;
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
+use yii\web\View;
 
-/* @var $this yii\web\View */
-/* @var $model app\models\Groupteacherbook */
-/* @var $form yii\widgets\ActiveForm */
+/**
+ * @var View       $this
+ * @var GroupBook  $model
+ * @var ActiveForm $form
+ * @var array      $books
+ */
 ?>
-<?php if (Yii::$app->session->get('user.ustatus')==3 || Yii::$app->session->get('user.ustatus')==4) : ?>
-<div class="groupteacherbook-form">
-
+<div class="group-book-form">
     <?php $form = ActiveForm::begin(); ?>
-
-    <?= $form->field($model, 'book_id')->dropDownList($item=$books, ['prompt'=>Yii::t('app', '-select-')]) ?>
-
-    <?= $form->field($model, 'primary')->checkbox(); ?>
-
-    <div class="form-group">
-        <?= Html::submitButton($model->isNewRecord ? Yii::t('app', 'Add') : 'Update', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
-    </div>
-
+        <?= $form->field($model, 'book_id')->dropDownList($books, ['prompt'=>Yii::t('app', '-select-')]) ?>
+        <?= $form->field($model, 'primary')->checkbox(); ?>
+        <div class="form-group">
+            <?= Html::submitButton($model->isNewRecord ? Yii::t('app', 'Add') : 'Update', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
+        </div>
     <?php ActiveForm::end(); ?>
-
-</div>
-<?php endif; ?>
-<div class="groupteacherboook-table">
-    <table class="table table-bordered table-striped">
-        <thead>
-            <tr>
-                <th>Наименование</th>
-                <th>Основной</th>
-                <th>Действ.</th>
-            </tr>
-        </thead>
-        <?php foreach($curr_books as $b) { ?>
-        <tr>
-            <td><?php echo $b['name']; ?></td>
-            <td><?php echo $b['prime'] ? '<span class="label label-primary"> ' . Yii::t('app', 'Prime') . '</span>' : ''; ?></td>
-            <td>
-            <?php echo Html::a('', ['delete', 'id'=>$b['id']], ['class' => 'glyphicon glyphicon-trash']); ?>
-            <?php echo !$b['prime'] ? Html::a('', ['primary', 'id'=>$b['id']], ['class' => 'glyphicon glyphicon-ok', 'title'=>Yii::t('app', 'Make primary')]) : ''; ?> 
-            </td>
-        </tr>
-        <?php } ?>
-    </table>
 </div>
