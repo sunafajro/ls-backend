d116a22fc1ca0fd3a385914f2c3d4a12ae4dd862
Evgeny Belkin <sunafajro@gmail.com>
2019-12-14 00:41:42 +0300

    #152 Восстановлена функция показа модальных окон сообщений

--- widgets/navigation/assets/js/navigation.js
+++ widgets/navigation/assets/js/navigation.js
@@ -1,43 +1,8 @@
 $(function() {
-  var $rootBlock = $("#navigation-panel");
-  var startTime = $rootBlock.data("limit-time") * 60;
-  var $counterBlock = $("#navigation-timer");
-
-  function decrementTime() {
-    startTime--;
-    if (startTime === 0) {
-      logout();
-    }
-    $counterBlock.text(timeFormat(startTime));
-    setTimeout(decrementTime, 1000);
-  }
-
-  function timeFormat(time) {
-    $minutes = ~~(time / 60);
-    $seconds = time % 60;
-    return (
-      ($minutes < 10 ? "0" + $minutes : $minutes) +
-      ":" +
-      ($seconds < 10 ? "0" + $seconds : $seconds)
-    );
-  }
-
-  function logout() {
-    $.ajax({
-      method: "POST",
-      url: $rootBlock.data("logout-url")
-    }).done(function() {
-      window.location.reload();
-    });
-  }
-
   $(document).ready(function() {
-    if (startTime > 0) {
-      decrementTime();
-      var $navMessageModal = $("#navigation-message-modal");
-      if ($navMessageModal.length) {
-        $navMessageModal.modal("show");
-      } 
-    }
+    var $navMessageModal = $("#navigation-message-modal");
+    if ($navMessageModal.length) {
+      $navMessageModal.modal("show");
+    } 
   });
 });
