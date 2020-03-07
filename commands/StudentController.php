0a6d29d9e5c9df7bba0562f8541ac5e014f1fd45
Evgeny Belkin <sunafajro@gmail.com>
2020-03-07 21:40:15 +0300

    Метод для обновления номеров телефонов студентов по данным из json файла

--- commands/StudentController.php
+++ commands/StudentController.php
@@ -5,6 +5,8 @@ namespace app\commands;
 use app\models\Student;
 use app\models\StudentCommission;
 use yii\console\Controller;
+use yii\helpers\Json;
+use Yii;
 
 class StudentController extends Controller
 {
@@ -38,6 +40,37 @@ class StudentController extends Controller
             if ($count % 100 === 0) {
                 echo "Updated " . $count . " rows\n";
             }
+        }
+    }
+
+    public function actionUpdatePhones()
+    {
+        $file = file_get_contents(Yii::getAlias('@app/data/csvjson.json'));
+
+        for ($i = 0; $i <= 31; ++$i) {
+            $file = str_replace(chr($i), "", $file);
+        }
+        $file = str_replace(chr(127), "", $file);
+
+        if (0 === strpos(bin2hex($file), 'efbbbf')) {
+            $file = substr($file, 3);
+        }
+
+        $students = Json::decode($file);
+        $t = Yii::$app->db->beginTransaction();
+        try {
+            foreach ($students as $student) {
+                $studentModel = Student::find()->andWhere(['id' => $student['id']])->one();
+                $studentModel->phone = (string)$student['phone'];
+                if (!$studentModel->save(true, ['phone'])) {
+                    throw new \Exception('Error on phones update!');
+                }
+            }
+            $t->commit();
+            echo "Success! ";
+        } catch (\Exception $e) {
+            $t->rollBack();
+            echo "Fail! " . $e->getMessage();
         }
     }
 }
