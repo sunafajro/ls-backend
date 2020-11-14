bdaa46c2c9bdf1c0be59f5b176d70beab13de712
Evgeny Belkin <sunafajro@gmail.com>
2020-11-14 12:53:10 +0300

    #201 Режим обслуживания сайта.

--- web/index.php
+++ web/index.php
@@ -4,16 +4,20 @@ use yii\helpers\ArrayHelper;
 defined('YII_DEBUG') or define('YII_DEBUG', isset($_SERVER['YII_DEBUG']) ? $_SERVER['YII_DEBUG'] : 'true');
 defined('YII_ENV') or define('YII_ENV', isset($_SERVER['YII_ENV']) ? $_SERVER['YII_ENV'] : 'dev');
 
-require(__DIR__ . '/../vendor/autoload.php');
-require(__DIR__ . '/../vendor/yiisoft/yii2/Yii.php');
+if (YII_ENV === 'maintenance') {
+    echo "В данный момент сайт находится в режиме обслуживания. Попробуйте зайти еще раз позже.";
+} else {
+    require(__DIR__ . '/../vendor/autoload.php');
+    require(__DIR__ . '/../vendor/yiisoft/yii2/Yii.php');
 
-$yiiActiveApp = isset($_SERVER['YII_ACTIVE_APP']) && $_SERVER['YII_ACTIVE_APP']
-    ? $_SERVER['YII_ACTIVE_APP']
-    : 'school';
+    $yiiActiveApp = isset($_SERVER['YII_ACTIVE_APP']) && $_SERVER['YII_ACTIVE_APP']
+        ? $_SERVER['YII_ACTIVE_APP']
+        : 'school';
 
-$config = ArrayHelper::merge(
-    require(__DIR__ . "/../config/web.php"),
-    require(__DIR__ . "/../config/{$yiiActiveApp}.php")
-);
+    $config = ArrayHelper::merge(
+        require(__DIR__ . "/../config/web.php"),
+        require(__DIR__ . "/../config/{$yiiActiveApp}.php")
+    );
 
-(new yii\web\Application($config))->run();
+    (new yii\web\Application($config))->run();
+}
