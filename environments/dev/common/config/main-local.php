7a1f8804410a652da6ee027244d709b7f1119406
Evgeny Belkin <sunafajro@gmail.com>
2021-02-14 00:20:18 +0300

    #223 Обновлено react приложение для создания счетов

--- environments/dev/common/config/main-local.php
+++ environments/dev/common/config/main-local.php
@@ -1,4 +1,5 @@
 <?php
+
 return [
     'components' => [
         'db' => [
@@ -11,9 +12,6 @@ return [
         'mailer' => [
             'class' => 'yii\swiftmailer\Mailer',
             'viewPath' => '@common/mail',
-            // send all mails to a file by default. You have to set
-            // 'useFileTransport' to false and configure a transport
-            // for the mailer to send real emails.
             'useFileTransport' => true,
         ],
     ],
