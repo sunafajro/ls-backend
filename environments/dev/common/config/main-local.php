be8beaae43139cc291df198651c8fc7bebb43b7b
Evgeny Belkin sunafajro@gmail.com <sunafajro@gmail.com>
2021-03-13 22:01:10 +0300

    Добавлены пустые папки для файлов и runtime для консольного приложения. Добавлены настройки БД в скрипт генерации конфигурации.

--- environments/dev/common/config/main-local.php
+++ environments/dev/common/config/main-local.php
@@ -4,9 +4,9 @@ return [
     'components' => [
         'db' => [
             'class' => 'yii\db\Connection',
-            'dsn' => 'mysql:host=localhost;dbname=yii2advanced',
-            'username' => 'root',
-            'password' => '',
+            'dsn' => 'mysql:host=localhost;dbname=langschool',
+            'username' => 'lsuser',
+            'password' => 'lsdbpass',
             'charset' => 'utf8',
         ],
         'mailer' => [
