24bc21cc32e08005509f725ae87b740d91af9e69
Evgeny Belkin <sunafajro@gmail.com>
2021-06-14 21:00:52 +0300

    #235 Фикс ошибки в отчете по опросам

--- school/views/report/poll/_totals.php
+++ school/views/report/poll/_totals.php
@@ -19,13 +19,13 @@ $questions = $poll->getQuestions()->andWhere(['visible' => 1])->indexBy('id')->a
                 $questionItems = ArrayHelper::index($question->items ?? [], 'id');
                 foreach($questionItems as $key => $questionItem) {
                     echo Html::beginTag('div');
-                    echo $key . '. ' . $questionItem['title'] . ': ' . $totals[$questionId][$key]['count'] ?? 0;
+                    echo $key . '. ' . $questionItem['title'] . ': ' . ($totals[$questionId][$key]['count'] ?? 0);
                     if (isset($questionItem['options']) && count($questionItem['options'])) {
                         echo Html::beginTag('div', ['style' => 'padding-left: 2rem']);
                         $questionSubItems = ArrayHelper::index($questionItem['options'] ?? [], 'id');
                         foreach($questionSubItems as $subKey => $subItem) {
                             echo Html::beginTag('div');
-                            echo $subKey . '. ' . $subItem['title'] . ': ' . $totals[$questionId][$key]['options'][$subKey] ?? 0;
+                            echo $subKey . '. ' . $subItem['title'] . ': ' . ($totals[$questionId][$key]['options'][$subKey] ?? 0);
                             echo Html::endTag('div');
                         }
                         echo Html::endTag('div');
